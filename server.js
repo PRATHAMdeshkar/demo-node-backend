a7be5fd15c89615558b5df51d1f24e74c6ad74a6
PRATHAMdeshkar <pratham.deshkar@gmail.com>
2024-09-05 17:02:59 +0530

    angular changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDb = require("./config/dbConnections");
 const dotenv = require("dotenv").config()
+var cors = require("cors");
 
 connectDb();
 
@@ -8,6 +9,7 @@ const app =express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/events", require("./routes/eventRoutes"));
 app.use("/api/users", require("./routes/usersRoutes"));
